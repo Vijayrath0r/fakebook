a4d0f98d933f9a78401e29bff4810ecbec3b7ea7
Vijay Rathor <vijay.rathor@mindruby.com>
2024-03-05 12:30:51 +0530

    url commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL)
-
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + process.env.MONGODB_URL);
-});
-
-// If the connection throws an error
-mongoose.connection.on('error', function (err) {
-    console.log('Mongoose default connection error: ' + err);
-}); 
+mongoose.connect("mongodb+srv://"+"vijayrathor"+":FXA9S6TgRwyYYGDX@mongo-mumbai.qiitxtg.mongodb.net/"+"fakebook?retryWrites=true&w=majority")
