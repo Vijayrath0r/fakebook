f49c1f0f974228cf974dc29f3b2f25978c981a41
Vijay Rathor <vijay.rathor@mindruby.com>
2024-04-30 13:06:52 +0530

    added code for env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://"+"vijayrathor"+":FXA9S6TgRwyYYGDX@mongo-mumbai.qiitxtg.mongodb.net/"+"fakebook?retryWrites=true&w=majority")
+console.log(process.env);
+mongoose.connect(process.env.MONGODB_URL);
